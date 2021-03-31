8342c423f225e18394c6683666f537cb8e2f2837
Jacob Stiles <jacobstiles@Jacobs-MBP.home>
2021-03-31 16:04:07 -0400

    all but one route working

--- client/src/services/reviews.js
+++ client/src/services/reviews.js
@@ -1,26 +1,11 @@
 import api from './api-config';
 
-export const getAllFoods = async () => {
-  const resp = await api.get('/foods');
+export const getAllReviews = async () => {
+  const resp = await api.get('/albums/:album_id/reviews');
   return resp.data;
 }
 
-export const getOneFood = async (id) => {
-  const resp = await api.get(`/foods/${id}`);
+export const postReview = async (reviewData) => {
+  const resp = await api.post('/albums/:album_id/reviews', { review: reviewData });
   return resp.data;
-}
-
-export const postFood = async (foodData) => {
-  const resp = await api.post('/foods', { food: foodData });
-  return resp.data;
-}
-
-export const putFood = async (id, foodData) => {
-  const resp = await api.put(`/foods/${id}`, { food: foodData });
-  return resp.data;
-}
-
-export const destroyFood = async (id) => {
-  const resp = await api.delete(`/foods/${id}`);
-  return resp;
 }
